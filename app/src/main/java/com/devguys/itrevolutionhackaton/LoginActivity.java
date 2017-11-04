d5db3b18a6b31eab7d42758b4d842a11449a1fce
sergeyboy <likeanyanorigin@gmail.com>
2017-11-04 11:08:38 +0200

    Added package

---
+++ app/src/main/java/com/devguys/itrevolutionhackaton/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.devguys.itrevolutionhackaton;
+
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by sergeyboy on 04.11.17.
+ */
+
+public class LoginActivity extends ActivityCompat {
+
+}
